a4435381b29cd95273f5012ee8e06d87b919fcb4
MoSalah07 <notway10@yahoo.com>
2025-05-17 21:40:06 +0300

    add baseurl

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:7000/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "https://video-call-real-time-production.up.railway.app/api"
+    : "/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
